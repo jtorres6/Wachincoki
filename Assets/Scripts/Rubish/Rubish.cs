7571b8f9be571b31e56fe01f220e07e63b262fab
salvacorts <salvacorts97@gmail.com>
2019-07-26 16:00:13 -0700

    Wave removes garbage

--- Assets/Scripts/Rubish/Rubish.cs
+++ Assets/Scripts/Rubish/Rubish.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class Rubish : MonoBehaviour {
     public GameObject prefab;
     public int value;
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Hola");
+        Debug.Log("Hit by " + other.gameObject.transform.tag);
+        if (other.gameObject.transform.tag == "Wave") {
+            Debug.Log("Hit by wave");
+            Destroy(prefab);
+        }
+    }
 }
--- Assets/Scripts/Rubish/RubishInstancer.cs
+++ Assets/Scripts/Rubish/RubishInstancer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RubishInstancer : MonoBehaviour
 {
+    const float y_offset = 0.5f;
+
     public InstanceArea area;
     public int instances;
     public Rubish[] rubishTypes;
@@ -20,9 +22,9 @@ public class RubishInstancer : MonoBehaviour
             int idx = Random.Range(0, rubishTypes.Length);
             Rubish rubishType = rubishTypes[idx];
 
-            int x = Random.Range(area.minX, area.maxX);
-            int y = Random.Range(area.minY, area.maxY);
-            int z = Random.Range(area.minZ, area.maxZ);
+            float x = Random.Range(area.minX, area.maxX);
+            float y = Random.Range(area.minY, area.maxY) + y_offset;
+            float z = Random.Range(area.minZ, area.maxZ);
 
             GameObject rubish = Instantiate(rubishType.prefab, new Vector3(x, y, z), Quaternion.identity);
             rubishInstances.Add(rubish);
