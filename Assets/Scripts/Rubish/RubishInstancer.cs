3f7d5cd8256ebb7e8d5079c45a96eb85926dc2e2
salvacorts <salvacorts97@gmail.com>
2019-07-26 14:34:34 -0700

    Spawn random rubish
    Spawn random rubish of different types within a designated area

---
+++ Assets/Scripts/Rubish/RubishInstancer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubishInstancer : MonoBehaviour
+{
+    public InstanceArea area;
+    public int instances;
+    public Rubish[] rubishTypes;
+
+    private List<GameObject> rubishInstances;
+
+    RubishInstancer() {
+        rubishInstances = new List<GameObject>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        for (int i = 0; i < instances; i++) {
+            int idx = Random.Range(0, rubishTypes.Length);
+            Rubish rubishType = rubishTypes[idx];
+
+            int x = Random.Range(area.minX, area.maxX);
+            int y = Random.Range(area.minY, area.maxY);
+            int z = Random.Range(area.minZ, area.maxZ);
+
+            GameObject rubish = Instantiate(rubishType.prefab, new Vector3(x, y, z), Quaternion.identity);
+            rubishInstances.Add(rubish);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
