d03df72ba3b5529385871e6921a661113dea2fba
salvacorts <salvacorts97@gmail.com>
2019-07-27 04:36:29 -0700

    Add ownership change

--- Assets/Scripts/Rubish/Rubish.cs
+++ Assets/Scripts/Rubish/Rubish.cs
@@ -11,11 +11,21 @@ public class Rubish : MonoBehaviour {
         if (other.gameObject.transform.tag == "Wave") {
             Debug.Log("Hit by wave. Remove " + value + " to Player " + ownership);
             Destroy(gameObject);
+            return;
         }
 
         if (other.gameObject.transform.tag == "TruckGarbage") {
             Debug.Log("Hit by Truck");
             Destroy(gameObject);
+            return;
+        }
+
+        if (other.gameObject.transform.tag == "ChangeOwnership") {
+            Debug.Log("Change ownership");
+            OwnershipChange othersOwnership = other.gameObject.GetComponent<OwnershipChange>();
+            ownership = othersOwnership.ownership;
+            Debug.Log("New ownership: " + ownership);
+            return;
         }
     }
 
