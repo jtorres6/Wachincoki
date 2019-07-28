4afd324baf6541c19e9b6c49e99b4a6e4312e0d3
salvacorts <salvacorts97@gmail.com>
2019-07-28 02:18:08 -0700

    GameManager Singleton

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -3,24 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class gameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static GameManager _instance = null;
 
-    private int player1HP = 100;
-    private int player2HP = 100;
+    public static GameManager Instance { get { return _instance; } }
+
+    private int player1HP;
+    private int player2HP;
     
     public Text text1;
     public Text text2;
 
-    private bool isRunning = false;
-    private bool gameOver = false;
+    private bool isRunning;
+    private bool gameOver;
 
-    private int winner = 0;
+    private int winner;
+
+    // https://unity3d.com/es/learn/tutorials/projects/2d-roguelike-tutorial/writing-game-manager?playlist=17150
+    void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Start()
     {
         winner = 0;
+        gameOver = false;
+        isRunning = false;
+        player1HP = 1000;
+        player2HP = 1000;
     }
 
     // Update is called once per frame
@@ -39,23 +56,32 @@ public class gameManager : MonoBehaviour
             // si el jugador tira el tarro al otro, pasa el propietario al otro
 
             // si el jugador tira el objeto al camion, sumale la mitad del valor de la vida
-
-            if (player1HP <= 0){
-                winner = 2;
-                gameOver = true;
-            }
-            else if (player2HP <= 0){
-                winner = 1;
-                gameOver = true;
-            }
-
-            if (gameOver == false){
-
-            }
-            else{
-                //muestra por la UI lo de quien ha ganado y despues isrunning = false
+            if (gameOver){
+                winner = player1HP > player2HP ? 1 : 2;
             }
         }
-        
+    }
+
+    public void DecreaseHP(int playerID, int value) {
+        if (playerID == 1) {
+            player1HP -= value;
+        } else {
+            player2HP -= value;
+        }
+
+        if (player1HP <= 0 || player2HP <= 0) {
+            gameOver = true;
+        }
+
+        Debug.Log("Player1 HP: " + player1HP);
+        Debug.Log("Player2 HP: " + player2HP);
+    }
+
+    public void IncreaseHP(int playerID, int value) {
+        if (playerID == 1) {
+            player1HP += value;
+        } else {
+            player2HP += value;
+        }
     }
 }
